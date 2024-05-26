f64d993b185966c83a49fcde919876cfd7a91bff
squw <squwsun@gmail.com>
2024-05-26 13:19:39 -0400

    update README and load data script

--- Load_Data_From_TSV.py
+++ Load_Data_From_TSV.py
@@ -6,7 +6,7 @@ from urllib.parse import quote
 
 # Create a password.env to store password: file has one line: PASSWORD = "your_password"
 # This line loads the env file created
-load_dotenv(r'C:\Users\squw-\My Drive\uWaterloo\CS 338\Final Project\password.env')
+load_dotenv(r'./password.env')
 
 # MySQL Creds, change to your own if necessary
 host = "localhost"
@@ -15,21 +15,22 @@ password = os.getenv('MY_PASSWORD')
 database_name = "imdb_data"
 
 
+
 # Tables to add
 table_names = ["name_basics", "title_akas", "title_basics", "title_crew", "title_episode", "title_pricipals", "title_ratings"]
 # Paths for the tsv files
-file_paths = [r"your_path_to_name.basics.tsv", 
-              r"your_path_to_title.akas.tsv", 
-              r"your_path_to_title.basics.tsv", 
-              r"your_path_to_title.crew.tsv", 
-              r"your_path_to_title.episode.tsv", 
-              r"your_path_to_title.principals.tsv", 
-              r"your_path_to_title.ratings.tsv"]
+file_paths = [r"./tmp/name.basics.tsv", 
+              r"./tmp/title.akas.tsv", 
+              r"./tmp/title.basics.tsv", 
+              r"./tmp/title.crew.tsv", 
+              r"./tmp/title.episode.tsv", 
+              r"./tmp/title.principals.tsv", 
+              r"./tmp/title.ratings.tsv"]
 
 # Function to load table given the table name and the TSV file path
 def load_table(table_name, file_path): 
     # Read the TSV file (limiting 100 lines for faster testing speed)
-    df = pd.read_csv(file_path, sep='\t', na_values='\\N', nrows=100)
+    df = pd.read_csv(file_path, sep='\t', na_values='\\N')
 
     # Replace NaN with None for SQL NULL values
     df = df.where(pd.notnull(df), None)
--- README.md
+++ README.md
@@ -18,4 +18,12 @@ Since we have TSV files from IMDB, we choose to use python to load the tables in
 We use sqlalchemy engine to connect to local mathine
 Details in 'Load_Data_From_TSV.py'
 
+1. Run 'get_tsv_files.py' to get the tsvs in a tmp folder (do not push this folder and anything inside to repo)
+2. Configure your own password.env as described in 'Load_Data_From_TSV.py'
+3. Run 'Load_Data_From_TSV.py' to load all the data into your local database
+
 # Accessing the loaded data:
+configure your own test_access_db.ipynb for your local machine to access the database
+
+# About the ./tmp folder
+Please do not push ./tmp folder and anything inside as it's a temporary folder to store the tsv files downloaded from the internet
