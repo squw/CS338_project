f0aca52a03ab30824556c13322a2f38b321276e0
Xihao Sun <x294sun@uwaterloo.ca>
2024-05-28 17:51:48 -0400

    adding load_title_akas.sql

---
+++ load_title_akas.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS title_akas (
+    titleId VARCHAR(255) PRIMARY KEY,
+    ordering INT,
+    title VARCHAR(255),
+    region VARCHAR(255),
+    language VARCHAR(255),
+    types VARCHAR(255),
+    attributes VARCHAR(255),
+    isOriginalTitle INT
+);
+
+LOAD DATA LOCAL INFILE 'C:/Users/squw-/OneDrive/Documents/GitHub/IMDB_TV_Show_Movie_System/tmp/title.akas.tsv' INTO
+TABLE title_akas FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' IGNORE 1 LINES (
+    titleId,
+    ordering,
+    title,
+    region,
+    language,
+    types,
+    attributes,
+    isOriginalTitle
+);
