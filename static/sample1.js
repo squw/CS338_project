f6747343316814aa1ae13f598bf9019d15b68c1c
Xihao Sun <x294sun@uwaterloo.ca>
2024-06-08 01:48:33 -0400

    adding test_web_app2.py and its dependent html and js

---
+++ static/sample1.js
@@ -0,0 +1,23 @@
+function fetchData() {
+    fetch('/data')
+        .then(response => response.json())
+        .then(data => {
+            let tableBody = document.querySelector('#titleCrewTable5R tbody');
+            
+            // Clear existing table rows
+            while (tableBody.firstChild) {
+                tableBody.removeChild(tableBody.firstChild);
+            }
+
+            // Insert new data
+            data.forEach(sample => {
+                let row = tableBody.insertRow();
+                let tconst = row.insertCell(0);
+                let directors = row.insertCell(1);
+                let writers = row.insertCell(2);
+                tconst.innerHTML = sample.tconst;
+                directors.innerHTML = sample.directors;
+                writers.innerHTML = sample.writers;
+            });
+        });
+}
