62267bd52a8907e562011bbf03d67289e4f25da0
Xihao Sun <x294sun@uwaterloo.ca>
2024-06-14 16:34:09 -0400

    adding flask try

---
+++ Flask_try/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from urllib.parse import quote
+from dotenv import load_dotenv
+from sqlalchemy import text
+# 'pip install pymysql cryptography' may be a required package for this program to run
+
+
+
+
+# MySQL Creds
+load_dotenv(r'./password.env')
+host = "localhost"
+username = "root"
+password = os.getenv('MY_PASSWORD')
+database_name = "imdb_data"
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{quote(username)}:{quote(password)}@{quote(host)}/{quote(database_name)}?charset=utf8mb4"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class TitleAkas(db.Model):
+    __tablename__ = 'title_akas'
+    titleId = db.Column(db.String(255), primary_key=True)
+    ordering = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    region = db.Column(db.String(255))
+    language = db.Column(db.String(255))
+    types = db.Column(db.String(255))
+    attributes = db.Column(db.String(255))
+    isOriginalTitle = db.Column(db.Boolean)
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/title_akas')
+def show_title_akas():
+    result = db.session.execute(text('SELECT * FROM title_akas'))
+    title_akas_entries = result.fetchall()
+    return render_template('title_akas.html', title_akas_entries=title_akas_entries)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
