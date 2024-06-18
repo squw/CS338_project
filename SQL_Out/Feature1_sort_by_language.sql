bca9bf7c1963a8a856410bbf369fa4c371284050
Helin Fang <fanghlalan@gmail.com>
2024-06-18 11:56:58 -0400

    Delete SQL_Out/Feature1_sort_by_language.sql

--- SQL_Out/Feature1_sort_by_language.sql
+++
@@ -1,6 +0,0 @@
-SELECT tb.primaryTitle, ta.language
-FROM title_basics tb
-JOIN title_akas ta ON tb.tconst = ta.titleId
-WHERE ta.language = 'selected_language';
-
--- Replace 'selected_language' with the actual language selected by the user.
