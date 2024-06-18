a2299acf57b6dc7bb611cac7307e338389f60ae4
Helin Fang <fanghlalan@gmail.com>
2024-06-18 11:47:51 -0400

    Delete SQL/Feature2_sort_by_region.sql

--- SQL/Feature2_sort_by_region.sql
+++
@@ -1,6 +0,0 @@
-SELECT tb.primaryTitle, ta.region
-FROM titleBasics tb
-JOIN titleAkas ta ON tb.tconst = ta.titleId
-WHERE ta.region = 'selected_region';
-
--- Replace 'selected_region' with the actual region selected by the user.
