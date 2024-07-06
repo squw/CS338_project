03cfeee8ce1bd95aa087479f69c4cbb51499f936
zimablue233 <1617687797@qq.com>
2024-07-06 15:56:48 -0400

    Delete SQL/Feature7_watch_history.sql

--- SQL/Feature7_watch_history.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE watch_history (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,  -- Assuming you have a user authentication system
-    movie_id VARCHAR(255),
-    watch_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (movie_id) REFERENCES title_basics(tconst)
-);
