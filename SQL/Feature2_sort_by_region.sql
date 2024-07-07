b2d93b39f340592ace2d5de2e2c6466f3a934e94
helin-fang <fanghlalan@gmail.com>
2024-07-07 01:17:44 -0400

    update

--- SQL/Feature2_sort_by_region.sql
+++ SQL/Feature2_sort_by_region.sql
@@ -1,6 +1,7 @@
 SELECT tb.primaryTitle, ta.region
-FROM titleBasics tb
-JOIN titleAkas ta ON tb.tconst = ta.titleId
-WHERE ta.region = 'selected_region';
+FROM title_basics tb
+JOIN title_akas ta ON tb.tconst = ta.titleId
+WHERE ta.region = :selected_region;
+
 
 -- Replace 'selected_region' with the actual region selected by the user.
