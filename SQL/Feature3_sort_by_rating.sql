4504f76a209dc6c7d6875c5feda175ff56065842
RainW12138 <1395008890yu@gmail.com>
2024-07-06 23:59:45 -0400

    Update feature 3

--- SQL/Feature3_sort_by_rating.sql
+++ SQL/Feature3_sort_by_rating.sql
@@ -1,4 +1,4 @@
-SELECT averageRating, numVotes, primaryTitle
+SELECT primaryTitle, averageRating, numVotes 
 FROM title_basics
 JOIN title_ratings ON title_basics.tconst = title_ratings.tconst
 WHERE numVotes >= 500
