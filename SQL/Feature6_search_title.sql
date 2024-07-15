8a44e76c43c40275d534f4ef0693edc273dc27ef
squwsun <squwsun@gmail.com>
2024-07-14 21:16:02 -0400

    increase the search title efficiency

--- SQL/Feature6_search_title.sql
+++ SQL/Feature6_search_title.sql
@@ -1,27 +1,18 @@
 SELECT DISTINCT
-    ta1.titleId,
-    ta1.title,
+    ta.titleId,
+    tb.primaryTitle,
     tb.startYear,
     tb.titleType,
     tr.averageRating,
     tr.numVotes
 FROM 
-    title_akas AS ta1
-JOIN 
-    title_akas AS ta2
-ON 
-    ta1.titleId = ta2.titleId
-JOIN
-    title_ratings AS tr
-ON
-    ta1.titleId = tr.tconst
-JOIN
-    title_basics tb
-ON
-    ta1.titleId = tb.tconst
-WHERE 
-    ta2.title LIKE :usr_input
-    AND ta1.isOriginalTitle = 1
-ORDER BY 
+    title_akas ta,
+    title_basics tb,
+    title_ratings tr
+WHERE
+    ta.titleId = tb.tconst AND
+    ta.titleId = tr.tconst AND
+    ta.title like '%john%'
+ORDER BY
     tr.numVotes DESC
 LIMIT 200;
