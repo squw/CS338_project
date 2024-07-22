6b0af564137949443ac2b11518fd71436ca0ced2
squwsun <squwsun@gmail.com>
2024-07-21 22:48:03 -0400

    improving visuals for title details

--- SQL/title_details.sql
+++ SQL/title_details.sql
@@ -3,23 +3,35 @@ SELECT
     tb.titleType, 
     CASE 
         WHEN tb.isAdult = 0 THEN 
-         'Everyone'
+            'Everyone'
         ELSE 
-         'Adult'
+            'Adult'
     END AS isAdult, 
     CASE 
         WHEN tb.startYear = 0000 THEN 
-         'Unknown'
+            'Unknown'
         ELSE 
-         tb.startYear
+            tb.startYear
     END AS startYear, 
     tb.endYear, 
     CASE 
         WHEN tb.runtimeMinutes >= 60 THEN 
-         CONCAT(FLOOR(tb.runtimeMinutes / 60), ' hour(s) and ', tb.runtimeMinutes % 60, ' minute(s)')
+            CONCAT(
+                FLOOR(tb.runtimeMinutes / 60), 
+                ' hour', 
+                CASE WHEN FLOOR(tb.runtimeMinutes / 60) > 1 THEN 's' ELSE '' END, 
+                ' and ', 
+                tb.runtimeMinutes % 60, 
+                ' minute', 
+                CASE WHEN tb.runtimeMinutes % 60 > 1 THEN 's' ELSE '' END
+            )
         ELSE 
-         CONCAT(tb.runtimeMinutes, ' minute(s)')
-    END AS runTime, 
+            CONCAT(
+                tb.runtimeMinutes, 
+                ' minute', 
+                CASE WHEN tb.runtimeMinutes > 1 THEN 's' ELSE '' END
+            )
+    END AS runTime,
     tb.genres
 FROM
     title_basics tb
