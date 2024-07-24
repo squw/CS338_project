92745c03dc21b07c43cd2ecdf64abfd57f70c5db
Helin Fang <fanghlalan@gmail.com>
2024-07-24 18:09:29 -0400

    Add files via upload

---
+++ SQL/Feature2_most_watching_by_region.sql
@@ -0,0 +1,14 @@
+SELECT
+    tb.tconst,
+    tb.primaryTitle,
+    ta.region,
+    tr.numVotes as watchCount
+FROM
+    title_basics AS tb
+    JOIN title_ratings AS tr ON tb.tconst = tr.tconst
+    JOIN title_akas AS ta ON tb.tconst = ta.titleId
+WHERE
+    ta.region = ':selected_region'
+ORDER BY
+    watchCount DESC
+LIMIT 20;
